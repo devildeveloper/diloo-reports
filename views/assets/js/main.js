11f7dc268fe65b9a55f74a3997790128d3b9f35f
jherson <jherson.o.s@hotmail.com>
2016-05-12 16:39:18 -0500

    fix:login

--- views/assets/js/main.js
+++ views/assets/js/main.js
@@ -1,3 +1,30 @@
+$(".changeStatus").dropdown({
+	onChange:function(value,text,$el){
+
+		if(value.split(">")[1]==='conectado'){
+			$.ajax({
+				method:'get',
+				url:'/employee/changeStatus',
+				data:{'status':'Conectado',id:$('#supervisorId').val() }
+			}).done(function(data){
+				console.log(data)
+			})
+		}else if (value.split(">")[1]==='desconectado'){
+			$.ajax({
+				method:'get',
+				url:'/employee/changeStatus',
+				data:{'status':'Desconectado',id:$('#supervisorId').val()}
+			}).done(function(data){
+				console.log(data)
+			})
+		}else if(value.split(">")[1]==='salir'){
+			window.location.href='/auth/logout'
+		}
+	}
+})
+$("#menu .item").popup({position : 'right center'});
+
+	
 function mostrar(){
 
 	var box = document.querySelector("[name='password']") ;
@@ -9,3 +36,4 @@ function mostrar(){
 	}
 
 }
+	
