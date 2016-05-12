e16dc1d087ed583d5112440d79836a2f236134a0
Miguel Augusto Aliaga Pantoja <maliaga.pantoja@gmail.com>
2016-05-12 17:00:29 -0500

    add: reports code status

--- api/controllers/company.js
+++ api/controllers/company.js
@@ -8,10 +8,45 @@ const list = function(request,reply){
 	var Sequelize = request.server.plugins['hapi-sequelize'].db.sequelize;
 	Sequelize.query('select * from category_name_id',{type: Sequelize.QueryTypes.SELECT})
 				.then(function(result){
-					reply(result);
+					return reply(result);
 				})
+}
+const find = function(request,reply){
+	var Sequelize = request.server.plugins['hapi-sequelize'].db.sequelize;
+	Sequelize.query("select * from company_info(?)",{replacements:[request.query.id],type: Sequelize.QueryTypes.SELECT})
+				.then(function(result){
+					return reply(result);
+				})
+				.error(function(e){
+					return reply(e);
+				})	
+}
+const activity=function(request,reply){
+	var Sequelize = request.server.plugins['hapi-sequelize'].db.sequelize;
+	var reportCode = request.query.code;
+	if(reportCode === '001'){
+		
+	}else if(reportCode === '002'){
+		
+	}else if(reportCode === '003'){
+	
+	}else if(reportCode === '004'){
+		Sequelize.query("select * from company_tickets(?,?)",{
+						replacements:[request.query.start,request.query.end],
+						type: Sequelize.QueryTypes.SELECT})
+				.then(function(result){
+					return reply(result);
+				})
+				.catch(function(e){
+					return reply(e);
+				})			
+	}else{
+		reply('invalid report code');
+	}
 }
 module.exports={
 	create:create
 	,list:list
+	,find:find
+	,activity:activity
 }
