cf139b8040465113a0af50ee3f935a0cbe8d8876
Miguel Augusto Aliaga Pantoja <maliaga.pantoja@gmail.com>
2016-05-20 10:08:52 -0500

    views

--- views/assets/js/main.js
+++ views/assets/js/main.js
@@ -1,39 +1,20 @@
-$(".changeStatus").dropdown({
-	onChange:function(value,text,$el){
+$(document).on("ready",function(){
+    $("select").dropdown({
+        allowAdditions: true
+        ,apiSettings: {
+            url: window.location.href+'company/list'
+        }        
+    });
 
-		if(value.split(">")[1]==='conectado'){
-			$.ajax({
-				method:'get',
-				url:'/employee/changeStatus',
-				data:{'status':'Conectado',id:$('#supervisorId').val() }
-			}).done(function(data){
-				console.log(data)
-			})
-		}else if (value.split(">")[1]==='desconectado'){
-			$.ajax({
-				method:'get',
-				url:'/employee/changeStatus',
-				data:{'status':'Desconectado',id:$('#supervisorId').val()}
-			}).done(function(data){
-				console.log(data)
-			})
-		}else if(value.split(">")[1]==='salir'){
-			window.location.href='/auth/logout'
-		}
-	}
+    $("#menu .item").popup({position : 'right center'});
+
 })
-$("#menu .item").popup({position : 'right center'});
-
-	
 function mostrar(){
-
-	var box = document.querySelector("[name='password']") ;
-	var attr = box.getAttribute('type');
-	if(attr=== "password"){
-		box.setAttribute('type','text')
-	}else{
-		box.setAttribute('type','password')
-	}
-
-}
-	
+    var box = document.querySelector("[name='password']") ;
+    var attr = box.getAttribute('type');
+    if(attr=== "password"){
+        box.setAttribute('type','text')
+    }else{
+        box.setAttribute('type','password')
+    }
+}  	
